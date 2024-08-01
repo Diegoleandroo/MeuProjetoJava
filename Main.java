beff122144c4f2f5ec711501b40a043cf75538f7
Diegoleandroo <141598449+Diegoleandroo@users.noreply.github.com>
2024-07-31 22:41:12 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+public class Main{
+	
+	public static void main(String [] args){
+		
+		
+		System.out.print("Olá mundo");
+		System.out.print("Bem-vindo ao Git");
+		printAdditionalMessage();
+	
+	}
+	
+	
+	
+	public static void printAdditionalMessage(){
+		
+		System.out.print("Está é´uma mensagem atualizada. ");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
